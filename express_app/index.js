1ba39073fdeed8a589544c5a5d0da4e49cfbb1f0
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-01-05 22:29:53 +0900

    2つのページが表示されるように

--- express_app/index.js
+++ express_app/index.js
@@ -1,8 +1,36 @@
 const express = require('express');
-const app = express();
+const ejs = require('ejs');
 
-app.get('/', (request, response) => {
-  response.send('Welcome to Express!');
+const app = express();
+// アプリケーションで使うレンダリングエンジンの設定
+app.engine('ejs', ejs.renderFile);
+// publicフォルダを参照できるように設定
+app.use(express.static('public'));
+
+// トップページ
+app.get('/', (req, res) => {
+  const msg = 'This is Index Page!<br>これはトップページです';
+  res.render('index.ejs', {
+    title: 'Index',
+    content: msg,
+    link: {
+      href: '/other',
+      text: '※別のページに移動'
+    }
+  });
+});
+
+// otherページ
+app.get('/other', (req, res) => {
+  const msg = 'This is Other Page!<br>これはその他ページです';
+  res.render('index.ejs', {
+    title: 'Other',
+    content: msg,
+    link: {
+      href: '/',
+      text: '※トップに戻る'
+    }
+  });
 });
 
 app.listen(3000, () => {
