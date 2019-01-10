8eba0001d5a85a5081ba2630b65dab78d4b30635
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-01-11 02:10:29 +0900

    レコードの新規作成

--- express_generator/ex-gen-app/routes/db.js
+++ express_generator/ex-gen-app/routes/db.js
@@ -18,15 +18,39 @@ router.get('/', function(req, res, next) {
   // データを取り出す
   connection.query('select * from userdata', (error, results, fields) => {
     if (error === null) {
-      console.log(fields);
       const data = {
         title: 'Mysql',
         content: results
       };
-      res.render('db', data);
+      res.render('db/index', data);
     }
   });
   // 接続を解除
+  connection.end();
+});
+
+// 新規ページへのアクセス
+router.get('/add', (req, res, next) => {
+  const data = {
+    title: 'Db/add',
+    content: '新しいレコードを入力してください'
+  };
+  res.render('db/add', data);
+});
+
+// 新規作成フォーム送信の処理
+router.post('/add', (req, res, next) => {
+  const data = { name, mail, age } = req.body;
+  // コネクションの用意
+  const connection = mysql.createConnection(MYSQL_SETTING);
+  // DB接続
+  connection.connect();
+  // データを登録
+  connection.query('insert into userdata set ?', data, (error, results, fields) => {
+    console.log(results);
+    res.redirect('/db');
+  });
+  // DB接続解除
   connection.end();
 });
 
