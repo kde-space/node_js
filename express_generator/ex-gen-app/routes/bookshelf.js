0325e30387258cb1f7ad9a8bb0721005ea5f7abf
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-01-12 01:44:41 +0900

    まずは通常通りレコード一覧表示

--- express_generator/ex-gen-app/routes/bookshelf.js
+++ express_generator/ex-gen-app/routes/bookshelf.js
@@ -1,12 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const MYSQL_SETTING = require('../db/setting');
 
 router.get('/', (req, res, next) => {
-  const data = {
-    title: 'Bookshelf',
-    content: 'this is bookshelf page.'
-  };
-  res.render('bookshelf/index', data);
+  const connection = mysql.createConnection(MYSQL_SETTING);
+  connection.connect();
+  connection.query('select * from userdata', (error, results, fields) => {
+    if (error === null) {
+      const data = {
+        title: 'Bookshelf',
+        content: results
+      };
+      res.render('bookshelf/index', data);
+    }
+  })
+  connection.end();
 });
 
 module.exports = router;
