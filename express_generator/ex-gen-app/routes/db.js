8b0cb8d5a8d0e56c7bffce68980db897627a85a5
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-01-12 00:15:23 +0900

    基本的なCRUD操作

--- express_generator/ex-gen-app/routes/db.js
+++ express_generator/ex-gen-app/routes/db.js
@@ -47,10 +47,87 @@ router.post('/add', (req, res, next) => {
   connection.connect();
   // データを登録
   connection.query('insert into userdata set ?', data, (error, results, fields) => {
-    console.log(results);
     res.redirect('/db');
   });
   // DB接続解除
+  connection.end();
+});
+
+// 指定レコードを表示
+router.get('/show', (req, res, next) => {
+  const id = req.query.id;
+  const connection = mysql.createConnection(MYSQL_SETTING);
+  connection.connect();
+  connection.query('select * from userdata where id=?', id, (error, results, fields) => {
+    if (error === null) {
+      const data = {
+        title: 'Db/show',
+        content: `id = ${id} のレコード`,
+        mydata: results[0]
+      };
+      res.render('db/show', data);
+    }
+  });
+  connection.end();
+});
+
+// 指定レコードを編集
+router.get('/edit', (req, res, next) => {
+  const id = req.query.id;
+  const connection = mysql.createConnection(MYSQL_SETTING);
+  connection.connect();
+  connection.query('select * from userdata where id = ?', id, (error, results, fields) => {
+    if (error === null) {
+      const data = {
+        title: 'Db/edit',
+        content: `id = ${id} のレコード`,
+        mydata: results[0]
+      };
+      res.render('db/edit', data);
+    }
+  });
+  connection.end();
+});
+
+// 編集フォーム送信の処理
+router.post('/edit', (req, res, next) => {
+  const { id, name, mail, age } = req.body;
+  const data = { name, mail, age };
+  const connection = mysql.createConnection(MYSQL_SETTING);
+  connection.connect();
+  connection.query('update userdata set ? where id = ?', [data, id], (error, results, fields) => {
+    res.redirect('/db');
+  });
+  connection.end();
+});
+
+// 削除ページを表示
+router.get('/delete', (req, res, next) => {
+  const id = req.query.id;
+  const connection = mysql.createConnection(MYSQL_SETTING);
+  connection.connect();
+  connection.query('select * from userdata where id = ?', id, (error, results, fields) => {
+    if (error === null) {
+      const data = {
+        title: 'Db/delete',
+        content: `id = ${id} のレコード`,
+        mydata: results[0]
+      };
+      res.render('db/delete', data);
+    }
+  });
+});
+
+// 削除送信の処理
+router.post('/delete', (req, res, next) => {
+  const id = req.body.id;
+  const connection = mysql.createConnection(MYSQL_SETTING);
+  connection.connect();
+  connection.query('delete from userdata where id = ?', id, (error, results, fields) => {
+    if (error === null) {
+      res.redirect('/db');
+    }
+  });
   connection.end();
 });
 
