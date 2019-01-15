ee00afb9ef12ba03eaf139305fb09b5f70511bb9
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-01-15 17:10:01 +0900

    新規追加、ID指定表示　作成

--- express_generator/ex-gen-app/routes/bookshelf.js
+++ express_generator/ex-gen-app/routes/bookshelf.js
@@ -18,4 +18,41 @@ router.get('/', (req, res, next) => {
   connection.end();
 });
 
+router.get('/add', (req, res, next) => {
+  const data = {
+    title: 'Bookshelf/add',
+    content: '新しいレコードを入力'
+  };
+  res.render('bookshelf/add', data);
+});
+
+router.post('/add', (req, res, next) => {
+  const data = { name, mail, age } = req.body;
+  const connection = mysql.createConnection(MYSQL_SETTING);
+  connection.connect();
+  connection.query('insert into userdata set ?', data, (error, results, fields) => {
+    if (error === null) {
+      res.redirect('/bookshelf');
+    }
+  });
+  connection.end();
+});
+
+router.get('/show', (req, res, next) => {
+  const id = req.query.id;
+  const connection = mysql.createConnection(MYSQL_SETTING);
+  connection.connect();
+  connection.query('select * from userdata where id = ?', id, (error, results, fields) => {
+    if (error === null) {
+      const data = {
+        title: 'Bookshelf/show',
+        content: `id = ${id} のレコード`,
+        mydata: results[0]
+      };
+      res.render('bookshelf/show', data);
+    }
+  });
+  connection.end();
+});
+
 module.exports = router;
